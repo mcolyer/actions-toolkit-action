7ccacd07440112f3de03875de134a5b5f459026f
Matt Colyer <mcolyer@github.com>
2019-02-25 16:51:45 -0800

    Add fixtures for all events

--- .github/actions/create.js
+++ .github/actions/create.js
@@ -1,8 +1,7 @@
 // Triggered when repository, branch, or tag is created.
 //
 // Event properties:
-//   ref_type - the type of objection that was created one of
-//     `repository`, `branch`, or `tag`
+//   ref_type - the type of objection that was created one of `branch`, or `tag`
 //   ref - The git ref (or `null` if only a repository was created).
 //   master_branch - The name of the repository's default branch (usually `master`).
 //   description - The repository's current description.
